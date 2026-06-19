61c8f251e15fa078c720ebe9d0edd0dd36c03812
szzzmanu <manuelalucena95@gmail.com>
2026-06-19 23:32:16 +0000

    Condição de aprovação

--- atividade.py
+++ atividade.py
@@ -1,6 +1,7 @@
 nota1 = float(input("Nota 1: "))
 nota2 = float(input("Nota 2: "))
+nota3 = float(input("Nota 3: "))
 
-media = (nota1 + nota2) / 2
+media = (nota1 + nota2 + nota3) / 3
 
 print(media)
